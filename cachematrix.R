9fcfae1df494da86844be016592c8a8a1b2c9148
Christine Chapman <cchapman@nesystems.com>
2015-08-23 16:59:01 -0400

    add-caching-inverse
    Adding code for caching inverse functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,27 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+makeCacheMatrix<-function(m=matrix()){
+	z<-NULL
+	set<-function(y) {
+		m<<-y
+		z<<-NULL
+		}
+		get<-function() m
+		setinv<-function(solve) z<<-solve
+		getinv<-function() z
+		list(set=set, get=get, setinv=setinv, getinv=getinv)
+	}
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(m,...) {
+	n<-m$getinv()
+	if(!is.null(n)) { message("getting cached matrix")
+		return(n)}
+		data<-m$get()
+		n<-solve(data)
+		m$setinv(n)
+		return(n)
 }
